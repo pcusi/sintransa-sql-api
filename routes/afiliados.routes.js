9ce5f816cee2951ed85d60b69456e0a87116ce53
Piero Abraham Cusi Ruiz <pcusir@gmail.com>
2021-04-02 18:38:15 -0500

    Fix Afiliados

--- routes/afiliados.routes.js
+++ routes/afiliados.routes.js
@@ -4,7 +4,7 @@ const _afCntrl = require('../controllers/afiliados');
 const auth = require('../middlewares/auth');
 
 //[Controlador Afiliado]
-router.get('/afiliados/listar', _afCntrl.listar);
+router.get('/afiliados/listar', auth.obtenerToken, _afCntrl.listar);
 router.post('/afiliados/agregar', auth.obtenerToken, _afCntrl.agregar);
 router.put('/afiliados/actualizar/:id', auth.obtenerToken, _afCntrl.actualizar);
 router.put('/afiliados/activar/:id', auth.obtenerToken, _afCntrl.activar);
